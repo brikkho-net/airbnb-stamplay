2cde873ee2e42bbfc843b711cd4778393cc903c1
Ben Cochrane <bencochrane1@me.com>
2015-11-27 09:22:04 +1100

    update nav header

--- app/app.routes.js
+++ app/app.routes.js
@@ -1,5 +1,4 @@
 angular.module('app.routes', [])
-
 .config(AppRoutes);
 
 function AppRoutes($stateProvider, $urlRouterProvider, $locationProvider) {
@@ -10,37 +9,54 @@ function AppRoutes($stateProvider, $urlRouterProvider, $locationProvider) {
   $stateProvider
   .state('home', {
     url: '/',
-    templateUrl: 'app/components/home/home.html',
+    views: {
+      content: { templateUrl: 'app/components/home/home.html' }
+    },
     controller: 'HomeCtrl as home'
   })
 
   .state('room', {
     url: '/room',
-    templateUrl: 'app/components/room/room.html',
+    views: {
+      nav: { templateUrl: 'app/components/nav/nav.html' },
+      content: { templateUrl: 'app/components/room/room.html' }
+    },
     controller: 'RoomCtrl as room'
   })
 
   .state('booking', {
     url: '/booking',
-    templateUrl: 'app/components/booking/booking.html',
+    views: {
+      nav: { templateUrl: 'app/components/nav/nav.html' },
+      content: { templateUrl: 'app/components/booking/booking.html' }
+    },
     controller: 'BookingCtrl as booking'
   })
 
   .state('authenticate', {
     url: '/authenticate',
-    templateUrl: 'app/components/authenticate/authenticate.html',
+    views: {
+      nav: { templateUrl: 'app/components/nav/nav.html' },
+      content: { templateUrl: 'app/components/authenticate/authenticate.html' }
+    },
     controller: 'AuthenticateCtrl as authenticate'
   })
 
   .state('profile', {
     url: '/profile',
-    templateUrl: 'app/components/profile/profile.html',
+    views: {
+      nav: { templateUrl: 'app/components/nav/nav.html' },
+      content: { templateUrl: 'app/components/profile/profile.html' }
+    },
     controller: 'ProfileCtrl as profile'
   })
 
   .state('admin', {
     url: '/admin',
-    templateUrl: 'app/components/admin/admin.html',
+    views: {
+      nav: { templateUrl: 'app/components/nav/nav.html' },
+      content: { templateUrl: 'app/components/admin/admin.html' }
+    },
     controller: 'AdminCtrl as admin'
   });
 }
---
+++ app/components/nav/nav.js
@@ -0,0 +1,7 @@
+angular
+    .module('app.nav', [])
+    .controller('NavCtrl', NavCtrl);
+
+function NavCtrl() {
+    var nav = this;
+}
